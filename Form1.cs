31ad308b4b3d9f092e9547bd58e83d2dd07dbad6
Daliiuss <46731380+Daliiuss@users.noreply.github.com>
2020-05-06 21:20:30 +0300

    comments
    Added few comments

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace TheMusic_player
 {
     public partial class Form1 : Form
@@ -32,15 +31,12 @@ namespace TheMusic_player
                 }
             }
         }
-
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
-
         }
-
+        //play button controls
         private void button2_Click(object sender, EventArgs e)
         {
-            // Play the song 
             if (listBox1.SelectedIndex != -1)
             {
                 axWindowsMediaPlayer1.URL = paths[listBox1.SelectedIndex];
@@ -49,15 +45,12 @@ namespace TheMusic_player
             {
                 MessageBox.Show("No files selected");
             }
-
         }
-
         private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
         {
         
         }
-
-
+        //remove button controls
         private void button3_Click(object sender, EventArgs e)
         {
             ListBox.SelectedObjectCollection selectedItems = new ListBox.SelectedObjectCollection(listBox1);
@@ -72,16 +65,14 @@ namespace TheMusic_player
                 MessageBox.Show("No files selected");
             }
         }
-
+        //stop button controls
         private void button4_Click(object sender, EventArgs e)
         {
             axWindowsMediaPlayer1.Ctlcontrols.stop();
         }
-
+        //list control
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-           
+        { 
         }
     }
 }
